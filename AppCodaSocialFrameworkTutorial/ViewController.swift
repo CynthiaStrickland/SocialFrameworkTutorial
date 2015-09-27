b16ebc6bdade709ffc593d75b0283d7570820381
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-09-26 19:00:13 -0700

    tutorial demo

--- AppCodaSocialFrameworkTutorial/ViewController.swift
+++ AppCodaSocialFrameworkTutorial/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var noteTextView: UITextView!
+  @IBAction func showShareOptions(sender: AnyObject) {
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    configureNoteTextView()
   }
 
+  func configureNoteTextView() {
+    noteTextView.layer.cornerRadius = 8.0
+    noteTextView.layer.borderColor = UIColor(white: 0.75, alpha: 0.5).CGColor
+    noteTextView.layer.borderWidth = 1.2
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
